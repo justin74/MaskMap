2783bf797db53a24655a73136133829efbca57e3
80005459 <justin.huang@want-want.com>
2020-03-05 13:59:58 +0800

    Test get device location.

--- app/src/main/java/com/justin/huang/maskmap/MapsActivity.kt
+++ app/src/main/java/com/justin/huang/maskmap/MapsActivity.kt
@@ -1,24 +1,48 @@
 package com.justin.huang.maskmap
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.OnCompleteListener
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
+
+
+
+const val REQUEST_CODE_LOCATION = 123
+const val DEFAULT_ZOOM = 15f
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    // A default location (Sydney, Australia) and default zoom to use when location permission is
+    // not granted.
+    private val mDefaultLocation = LatLng(-33.8523341, 151.2106085)
     private lateinit var mMap: GoogleMap
+    private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
+    private var mLastKnownLocation: Location? = null
+
+    companion object {
+        private const val TAG = "MapsActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
+            .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
@@ -32,11 +56,69 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
+        // Return early if map is not initialised properly
         mMap = googleMap
+        enableMyLocation()
+    }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    /**
+     * enableMyLocation() will enable the location of the map if the user has given permission
+     * for the app to access their device location.
+     * Android Studio requires an explicit check before setting map.isMyLocationEnabled to true
+     * but we are using EasyPermissions to handle it so we can suppress the "MissingPermission"
+     * check.
+     */
+    @SuppressLint("MissingPermission")
+    @AfterPermissionGranted(REQUEST_CODE_LOCATION)
+    private fun enableMyLocation() {
+        if (hasLocationPermission()) {
+            mMap.isMyLocationEnabled = true
+            getDeviceLocation()
+        } else {
+            EasyPermissions.requestPermissions(
+                this, getString(R.string.location_require),
+                REQUEST_CODE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
+    private fun hasLocationPermission(): Boolean {
+        return EasyPermissions.hasPermissions(this, Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
+    private fun getDeviceLocation() {
+        val locationResult = mFusedLocationProviderClient.lastLocation
+        locationResult.addOnCompleteListener(this, OnCompleteListener { task ->
+            if (task.isSuccessful) {
+                mLastKnownLocation = task.result
+                if (mLastKnownLocation != null) {
+                    val lat = mLastKnownLocation!!.latitude
+                    val lng = mLastKnownLocation!!.longitude
+                    Log.e(TAG, "lat = $lat, lng = $lng")
+                    val latLng = LatLng(lat, lng)
+                    moveToLocation(latLng)
+                } else {
+                    Log.d(TAG, "Current location is null. Using defaults.")
+                    Log.e(TAG, "Exception: %s", task.exception)
+                    moveToLocation(mDefaultLocation)
+                    mMap.uiSettings.isMyLocationButtonEnabled = false
+                }
+            }
+        })
+    }
+
+    private fun moveToLocation(latLng: LatLng) {
+       CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM).let {
+           mMap.moveCamera(it)
+       }
     }
 }
